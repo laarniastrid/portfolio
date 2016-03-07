e7ab402d1d7369c07c0a92a988d3570f46a17e1d
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-06 19:17:31 -0700

    did stuff

--- directs/mainDir.js
+++ directs/mainDir.js
@@ -3,8 +3,8 @@ angular.module('myApp')
 .directive('mainDir', function() {
 
   return {
-    restrict: 'E',
-    templateUrl: 'http://laarniastrid.github.io/portfolio/views/main.html',
+    restrict: 'AE',
+    templateUrl: './views/main.html',
   }
 
 
