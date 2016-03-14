bf3fced167ee8654081f4299cdb3ff691db6100a
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-13 20:25:34 -0600

    added link

--- directs/circlesDir.js
+++ directs/circlesDir.js
@@ -5,6 +5,12 @@ angular.module('myApp')
   return {
     restrict: 'E',
     templateUrl: 'views/circles.html',
+    controller: function($scope) {
+       $scope.whalesSite = function() {
+        //  href='http://laarniastrid.com/noServer';
+        window.location.href = 'http://laarniastrid.com/noServer';
+       }
+    }
   }
 
 
---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  // alert('hi there');
+
+  // $('whaleSite').on('click', function() {
+    // $('<a href="http://laarniastrid.com/noServer"></a>').click();
+    // location.href = 'http://laarniastrid.com/noServer';
+    // document.location.href = 'http://laarniastrid.com/noServer';
+    // alert('you hovered!');
+  // })
+
+}) // end document ready
