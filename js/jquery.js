0cf1cfa4da31fa0806abebb7433a50cc4be67dcc
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-14 01:02:04 -0600

    testing styles

--- js/jquery.js
+++ js/jquery.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 
   // alert('hi there');
 
+  $('.nav-media').on('click', function() {
+    $('.nav-bar-top').css({
+      'display': 'flex'
+    })
+  })
 
 
 }) // end document ready
