486575b84222256b283f8226fef619046bbeb737
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-14 01:10:26 -0600

    testing bottom nav media

---
+++ directs/navBot.js
@@ -0,0 +1,32 @@
+angular.module('myApp')
+
+.directive('navBot', function() {
+
+  return {
+    restrict: 'E',
+    templateUrl: 'views/navBot.html',
+    // controller: 'mainCtrl',
+    scope: {
+      icon: '=',
+      link: '='
+    },
+    link: function(scope, ele, attr) {
+      ele.on('mouseover', function() {
+        $('.icon-hover', this).css({
+          'box-shadow': '0px 0px 20px #006699',
+          'border-radius': '5px',
+          'background': '#45A6D6',
+        })
+      })
+      ele.on('mouseout', function() {
+        $('.icon-hover', this).css({
+          'box-shadow': 'none',
+          'border-radius': 'none',
+          'background': 'none',
+        })
+      })
+    }
+
+  }
+
+}) // end of navDir
