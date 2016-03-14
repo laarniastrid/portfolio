37b49f371d27031180f559be5a5bced1da87ea8f
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-14 13:50:27 -0600

    testing media query styling

--- js/jquery.js
+++ js/jquery.js
@@ -2,11 +2,36 @@ $(document).ready(function() {
 
   // alert('hi there');
 
-  // $('.nav-media').on('click', function() {
-  //   $('.nav-bar-top').css({
-  //     'display': 'flex'
-  //   })
-  // })
+
+  $('.nav-media').on('click', function() {
+    $('.nav-bar-top').css({
+      'display': 'flex'
+    })
+  })
+
+
+
+
+  /*
+  .nav-bar {
+    width: auto;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    right: 0;
+
+    padding-right: 20px;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    text-align: center;
+    align-items: center;
+
+    .nav-bar-top {
+      margin-top: 10px;
+    }
+  }
+  */
 
 
 }) // end document ready
