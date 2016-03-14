c941b762036c31455367eff9e161f962f2eec7ce
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-13 23:49:06 -0600

    added new dir for nav bottom and updated links for social sites

--- controls/mainCtrl.js
+++ controls/mainCtrl.js
@@ -2,7 +2,7 @@ angular.module('myApp')
 
 .controller('mainCtrl', function($scope) {
 
-  // $scope.test = "this is a test";
+  // $scope.test = 'this is a test';
 
   /* ----- nav top buttons */
   // home button
@@ -11,55 +11,55 @@ angular.module('myApp')
   $scope.homeRoute = 'home';
 
   // sketches button
-  $scope.sketchVal = "Sketches";
-  $scope.sketchIcon = "fa fa-paint-brush fa-3x";
-  $scope.sketchRoute = "sketches";
+  $scope.sketchVal = 'Sketches';
+  $scope.sketchIcon = 'fa fa-paint-brush fa-3x';
+  $scope.sketchRoute = 'sketches';
 
   // skills button
-  $scope.skillsVal = "Skills";
-  $scope.skillsIcon = "fa fa-gear fa-3x";
-  $scope.skillsRoute = "skills";
+  $scope.skillsVal = 'Skills';
+  $scope.skillsIcon = 'fa fa-gear fa-3x';
+  $scope.skillsRoute = 'skills';
 
   // contact button
-  $scope.contactVal = "Contact";
-  $scope.contactIcon = "fa fa-envelope-o fa-3x";
-  $scope.contactRoute = "contact";
+  $scope.contactVal = 'Contact';
+  $scope.contactIcon = 'fa fa-envelope-o fa-3x';
+  $scope.contactRoute = 'contact';
 
-  /* ---------- nav bottom buttons ---------- */
+  /* nav bottom buttons */
   // github button
-  // $scope.gitVal = "Github";
-  $scope.gitIcon = "fa fa-github-alt fa-3x";
-  // $scope.gitRoute = "home";
+  // $scope.gitVal = 'Github';
+  $scope.gitIcon = 'fa fa-github-alt fa-3x';
+  $scope.gitRoute = 'https://github.com/laarniastrid';
 
   // linkedin button
-  // $scope.linkedVal = "LinkedIn";
-  $scope.linkedIcon = "fa fa-linkedin fa-3x";
-  // $scope.linkedRoute = "home";
+  // $scope.linkedVal = 'LinkedIn';
+  $scope.linkedIcon = 'fa fa-linkedin fa-3x';
+  $scope.linkedRoute = 'https://www.linkedin.com/in/laarniastrid';
 
   // twitter button
-  // $scope.twitVal = "Twitter";
-  $scope.twitIcon = "fa fa-twitter fa-3x";
-  // $scope.twitRoute = "home";
+  // $scope.twitVal = 'Twitter';
+  $scope.twitIcon = 'fa fa-twitter fa-3x';
+  $scope.twitRoute = 'https://twitter.com/laarniastrid';
 
   // youtube button
-  // $scope.youtVal = "YouTube";
-  $scope.youtubeIcon = "fa fa-youtube-play fa-3x";
-  // $scope.youtRoute = "home";
+  // $scope.youtVal = 'YouTube';
+  $scope.youtubeIcon = 'fa fa-youtube-play fa-3x';
+  $scope.youtRoute = 'https://www.youtube.com/user/HardsuitLoL';
 
 
 // trying contact control here.controller('contactCtrl', function($scope) {
 
   // $scope.addPost = function(data) {
   //   $.ajax({
-  //       url: "http://formspree.io/ecriyliz.yilrjsjy@gmail.com",
-  //       method: "POST",
+  //       url: 'http://formspree.io/ecriyliz.yilrjsjy@gmail.com',
+  //       method: 'POST',
   //       data: {
   //         name: data.name,
   //         email: data.email,
   //         phone: data.phone,
   //         message: data.text,
   //       },
-  //       dataType: "json"
+  //       dataType: 'json'
   //   });
   //   // alert('there are bees!');
   //   console.log($scope.contact);
