7103567c597996740b15b9c6f7c9b87614eb708f
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-14 01:08:39 -0600

    testing media queries

--- js/jquery.js
+++ js/jquery.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
   // alert('hi there');
 
-  $('.nav-media').on('click', function() {
-    $('.nav-bar-top').css({
-      'display': 'flex'
-    })
-  })
+  // $('.nav-media').on('click', function() {
+  //   $('.nav-bar-top').css({
+  //     'display': 'flex'
+  //   })
+  // })
 
 
 }) // end document ready
