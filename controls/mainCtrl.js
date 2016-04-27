a40d9400c2c330d31e11dbf2106f6929c02978b9
Laarni Astrid <laarni.astrid@gmail.com>
2016-04-27 13:06:45 -0600

    added skills icons

--- controls/mainCtrl.js
+++ controls/mainCtrl.js
@@ -2,8 +2,6 @@ angular.module('myApp')
 
 .controller('mainCtrl', function($scope) {
 
-  // $scope.test = 'this is a test';
-
   /* ----- nav top buttons */
   // home button
   $scope.homeVal = 'Home';
@@ -27,44 +25,20 @@ angular.module('myApp')
 
   /* nav bottom buttons */
   // github button
-  // $scope.gitVal = 'Github';
   $scope.gitIcon = 'fa fa-github-alt fa-3x';
   $scope.gitRoute = 'https://github.com/laarniastrid';
 
   // linkedin button
-  // $scope.linkedVal = 'LinkedIn';
   $scope.linkedIcon = 'fa fa-linkedin fa-3x';
   $scope.linkedRoute = 'https://www.linkedin.com/in/laarniastrid';
 
   // twitter button
-  // $scope.twitVal = 'Twitter';
   $scope.twitIcon = 'fa fa-twitter fa-3x';
   $scope.twitRoute = 'https://twitter.com/laarniastrid';
 
   // youtube button
-  // $scope.youtVal = 'YouTube';
   $scope.youtubeIcon = 'fa fa-youtube-play fa-3x';
   $scope.youtRoute = 'https://www.youtube.com/user/HardsuitLoL';
-
-
-// trying contact control here.controller('contactCtrl', function($scope) {
-
-  // $scope.addPost = function(data) {
-  //   $.ajax({
-  //       url: 'http://formspree.io/ecriyliz.yilrjsjy@gmail.com',
-  //       method: 'POST',
-  //       data: {
-  //         name: data.name,
-  //         email: data.email,
-  //         phone: data.phone,
-  //         message: data.text,
-  //       },
-  //       dataType: 'json'
-  //   });
-  //   // alert('there are bees!');
-  //   console.log($scope.contact);
-  //   $scope.contact = '';
-  // }
 
 
 
